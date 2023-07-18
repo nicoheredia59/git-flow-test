82c267ed5650ea642b6b62ad57b463aa40c9a137
nicoheredia59 <nicoheredia59@gmail.com>
2023-07-18 20:18:04 -0300

    refactor: update root page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,20 @@
+'use client'
+
 import * as React from 'react'
 
 export default function RootPage () {
+  // Prevent Hydration conflicts
+  const [mounted, setIsMounted] = React.useState(false)
+
+  React.useEffect(() => {
+    setIsMounted(true)
+  }, [])
+
+  if (!mounted) return null
+
   return (
-    <div>
-      root
-    </div>
+    <main className='grid place-items-center'>
+      Root Page
+    </main>
   )
 }
